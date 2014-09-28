ccccbacb975066728938900b11cc45af9e2060bc
Kleber Stender <kleber.stender@gmail.com>
2014-09-28 13:36:27 -0300

    Created Monad

--- typeclasses/Functor.php
+++ typeclasses/Functor.php
@@ -6,5 +6,10 @@
 require_once '../Fn/Fn1.php';
 
 interface Functor {
+    /**
+     * Function to traverse the container and apply a function to transform it
+     * f a -> (a -> b) -> f b
+     * @param Fn1 $f
+     */
     public function map(Fn1 $f);
 }
---
+++ typeclasses/Monad.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Description of Monad
+ *
+ * @author sirkleber
+ */
+require_once '../Fn/Fn1.php';
+require_once './Functor.php';
+
+abstract class Monad extends Functor{
+    
+    /**
+     * Haskell >>= (bind) function
+     * flatMap:: m a -> (a -> m b) -> m b
+     * @param Fn1 $f
+     */
+    public abstract function flatMap(Fn1 $f);
+    
+    /**
+     * Haskell >> function
+     * @param Fn1 $f
+     */
+    public function fpForeach(Fn1 $f){
+        $this->map($f);
+    }
+}
