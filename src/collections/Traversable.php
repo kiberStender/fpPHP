2b1f131d22d27b8e87270bd2d6d4c1879a8369bc
Kleber Stender <kleber.stender@gmail.com>
2014-09-28 17:39:02 -0300

    Created Traversable abstract class

---
+++ src/collections/Traversable.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Description of Traversable
+ *
+ * @author sirkleber
+ */
+
+set_include_path(dirname(__FILE__) . "/../");
+
+require_once 'fn/Fn1.php';
+require_once 'typeclasses/Monad.php';
+
+abstract class Traversable extends Monad{
+    //put your code here
+}
