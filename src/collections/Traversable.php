9ee175a2c6e51f035eed25b7335a305fab213ccc
Kleber Stender <kleber.stender@gmail.com>
2014-09-28 21:47:27 -0300

    Added Traversable methods using PHP 5.2 limitations

--- src/collections/Traversable.php
+++ src/collections/Traversable.php
@@ -9,8 +9,162 @@
 set_include_path(dirname(__FILE__) . "/../");
 
 require_once 'fn/Fn1.php';
+require_once 'fn/Fn2.php';
 require_once 'typeclasses/Monad.php';
 
 abstract class Traversable extends Monad{
-    //put your code here
+    /**
+     * @return Boolean Description
+     */
+    public abstract function isEmpty();
+    
+    /**
+     * @return A The item itself
+     */
+    public abstract function head();
+    
+    /**
+     * @return Traversable The array continuation
+     */
+    public abstract function tail();
+    
+    /**
+     * @return Traversable The array continuation
+     */
+    public abstract function init();
+
+    
+    /**
+     * @return A The last irem of the array
+     */
+    public abstract function last();
+    
+    /**
+     * @return Maybe If there is a head, it return a Just with it
+     */
+    public abstract function maybeHead();
+
+    /**
+     * @return Maybe If there is a last, it return a Just with it
+     */
+    public abstract function maybeLast();
+
+    /**
+     * @return Traversable 
+     */
+    protected abstract function empty_();
+
+    /**
+     * Scala :: and Haskell : functions
+     * @param item the item to be appended to the collection
+     * @return a new collection
+     */
+    public abstract function cons($item);
+
+    /**
+     * Scala and Haskell ++ function
+     * @param prefix new collection to be concat in the end of this collection
+     * @return a new collection
+     */
+    public abstract function concat(Traversable $prefix);
+    
+    public final function __toString(){
+        return "{$this->prefix()}({$this->foldLeft("", new ToStringFrm($this->toStringFrmt()))}))";
+    }
+
+    protected abstract function prefix();
+
+    protected abstract function toStringFrmt();
+
+    
+    public function length(){
+        return $this->foldLeft(0, new SumFoldLeft());
+    }
+
+    public function filter(Fn1 $p){
+        return $this->foldRight($this->empty_(), new FilterFoldRight($p));
+    }
+
+    public function filterNot(Fn1 $p){
+        return $this->filter(new FilterNot($p));
+    }
+
+    public final function partition(Fn1 $p){
+        return array($this->filter($p), $this->filterNot($p));
+    }
+
+    public function find(Fn1 $p){
+        if($this->isEmpty()){
+            return Nothing::Nothing();
+        } else {
+            if($p->apply($this->head())){
+                return new Just($this->head());
+            } else {
+                return $this->tail()->find($p);
+            }
+        }
+    }
+
+    public abstract function splitAt($n);
+    
+    public function foldLeft($acc, Fn2 $f){
+        if($this->isEmpty()){
+            return $acc;
+        } else {
+            return $this->tail()->foldLeft($f->apply($acc, $this->head()), $f);
+        }
+    }
+
+    public function foldRight($acc, Fn2 $f){
+        if(isEmpty()){
+            return acc;
+        } else {
+            return $f->apply($this->head(), $this->tail()->foldRight($acc, $f));
+        }
+    }
+}
+
+class ToStringFrm implements Fn2{
+    private $frmt;
+    
+    function __construct(Fn2 $frmt) {
+        $this->frmt = $frmt;
+    }
+    
+    public function apply($acc, $item) {
+        return $this->frmt->apply($acc, $item);
+    }
+}
+
+class SumFoldLeft implements Fn2{
+    public function apply($acc, $item) {
+        return $acc + $item;
+    }
+}
+
+class FilterFoldRight implements Fn2{
+    private $p;
+    
+    public function __construct(Fn1 $p) {
+        $this->p = $p;
+    }
+
+    public function apply($item, Traversable $acc) {
+        if($this->p->apply($item)){
+            return $acc->cons($item);
+        } else {
+            return $acc;
+        }
+    }
+}
+
+class FilterNot implements Fn1{
+    private $p;
+    
+    public function __construct(Fn1 $p) {
+        $this->p = $p;
+    }
+    public function apply($item) {
+        return !$this->p->apply($item);
+    }
 }
