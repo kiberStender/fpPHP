76889a3847546f54f4b5413f5afc7cee824d0402
sirkleber <kleber.stender@gmail.com>
2014-10-15 16:55:47 -0300

    Updated to php 5.3

--- src/db/FDB.php
+++ src/db/FDB.php
@@ -5,9 +5,14 @@
  *
  * @author sirkleber
  */
+namespace db;
+
 set_include_path(dirname(__FILE__) . "/../");
 
 require_once 'maybe/Maybe.php';
+
+use maybe\Just;
+use maybe\Nothing;
 
 class FDB {
   
@@ -33,7 +38,23 @@ class FDB {
    * @return Maybe
    */
   private final static function connect(){
-    return self::getDBConfig()->flatMap(new GetDbConfFlatMap());
+    return self::getDBConfig()->flatMap(function($json){
+      $dsn = call_user_func(function() use($json){
+        if ($json->db === ""){
+          return "{$json->dbms}:host={$json->host};port={$json->port};";
+        } else{
+          return "{$json->dbms}:host={$json->host};dbname={$json->db};port={$json->port};";
+        }
+      });
+      
+      try {
+        $pdo = new PDO($dsn, $json->username, $json->passwd);
+        
+        return new Just(array($pdo, null));
+      }catch (PDOException $ex) {
+        return new Just(array(null, $ex->getMessage()));
+      }
+    });
   }
   
   private final static function disconnect(array $arr){
@@ -46,51 +67,20 @@ class FDB {
   
   /**
    * 
-   * @param Fn1 $fn
+   * @param $fn
    * @version 3.5
    * @return Maybe
    */
-  public static function withConnection(Fn1 $fn){
-    return self::disconnect(self::connect()->flatMap(new ConnectFlatMap($fn)));
-  }
-  
-}
-
-class GetDbConfFlatMap implements Fn1{
-  public function apply($json) {
-    $dsn = "";
-    
-    if ($json->db == ""){
-      $dsn = "{$json->dbms}:host={$json->host};port={$json->port};";
-    } else{
-      $dsn = "{$json->dbms}:host={$json->host};dbname={$json->db};port={$json->port};";
-    }
-    
-    try {
-      $pdo = new PDO($dsn, $json->username, $json->passwd);
+  public static function withConnection($fn){
+    return self::disconnect(self::connect()->flatMap(function($tuple) use($fn){
+      $pdo = $tuple[0];
+      $ex = $tuple[1];
       
-      return new Just(array($pdo, null));
-    } catch (PDOException $ex) {
-      return new Just(array(null, $ex->getMessage()));
-    }
-  }
-}
-
-class ConnectFlatMap implements Fn1{
-  private $fn;
-  
-  function __construct(Fn1 $fn) {
-    $this->fn = $fn;
-  }
-
-  public function apply($tuple) {
-    $pdo = $tuple[0];
-    $ex = $tuple[1];
-    
-    if(isset($pdo) && $pdo instanceof PDO){
-      return array($pdo, $this->fn->apply($pdo));
-    } else {
-      return array($pdo, $ex);
-    }
+      if(isset($pdo) && $pdo instanceof PDO){
+        return array($pdo, $fn($pdo));
+      } else {
+        return array($pdo, $ex);
+      }
+    }));
   }
 }
