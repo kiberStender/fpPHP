9ec8ce3f1bde00c6ceade89174b12e3514e27105
sirkleber <kleber.stender@gmail.com>
2014-10-28 20:44:07 -0200

    Changed comment

--- src/typeclasses/Functor.php
+++ src/typeclasses/Functor.php
@@ -10,7 +10,7 @@ interface Functor {
     /**
      * Function to traverse the container and apply a function to transform it
      * f a -> (a -> b) -> f b
-     * @param Fn1 $f
+     * @param (a -> b) $f
      */
     public function map($f);
 }
