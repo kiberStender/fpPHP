1e6be203edda01beecd632e2cfe435c258058b29
sirkleber <kleber.stender@gmail.com>
2014-11-01 16:13:21 -0200

    Renamed Maybe file to Maybe.class

--- src/fp/maybe/Maybe.class.php
+++ src/fp/maybe/Maybe.class.php
@@ -5,31 +5,27 @@
  *
  * @author sirkleber
  */
-set_include_path(dirname(__FILE__) . "/../");
-
-require_once "fn/Fn.php";
-require_once 'typeclasses/Monad.php';
 
 abstract class Maybe extends Monad{
-    public function map(Fn1 $f) {
-        if($this instanceof Nothing){
-            return $this;
-        } else {
-            return new Just($f->apply($this->get()));
-        }
+  public function map(Fn1 $f) {
+    if($this instanceof Nothing){
+      return $this;
+    } else {
+      return new Just($f->apply($this->get()));
     }
-    
-    public function flatMap(Fn1 $f) {
-        if($this instanceof Nothing){
-            return $this;
-        } else {
-            return $f->apply($this->get());
-        }
-    }
-    
-    public abstract function getOrElse(Fn $f);
-    
-    public abstract function get();
+  }
+  
+  public function flatMap(Fn1 $f) {
+    if($this instanceof Nothing){
+      return $this;
+      } else {
+        return $f->apply($this->get());
+      }
+  }
+  
+  public abstract function getOrElse(Fn $f);
+  
+  public abstract function get();
 }
 
 class Just extends Maybe{
