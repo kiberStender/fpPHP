1e58e1c447c2f26d1bcbecb8ee9e1ce826ac9931
sirkleber <kleber.stender@gmail.com>
2014-11-01 21:23:46 -0200

    Added collections seq to autoloader

--- src/autoloader.php
+++ src/autoloader.php
@@ -2,20 +2,28 @@
   function my_autoloader($class) {
     $arr = array(
       #fn
-      "Fn" => "fn/Fn.class.php",
-      "Fn1" => "fn/Fn1.class.php",
-      "Fn2" => "fn/Fn2.class.php",
+      "Fn" => "fn/Fn",
+      "Fn1" => "fn/Fn1",
+      "Fn2" => "fn/Fn2",
       #typeclasses
-      "Functor" => "typeclasses/Functor.class.php",
-      "Monad" => "typeclasses/Monad.class.php",
+      "Functor" => "typeclasses/Functor",
+      "Monad" => "typeclasses/Monad",
       #Maybe
-      "Just" => "maybe/Just.class.php",
-      "Maybe" => "maybe/Maybe.class.php",
-      "Nothing" => "maybe/Nothing.class.php"
+      "Just" => "maybe/Just",
+      "Maybe" => "maybe/Maybe",
+      "Nothing" => "maybe/Nothing",
+      #collections
+      "FTraversable" => "collections/FTraversable",
+      #collections.seq
+      "Cons" => "collections/seq/Cons",
+      "Nil" => "collections/seq/Nil",
+      "Seq" => "collections/seq/Seq"
+      #collections.map
+      #collections.set
     );
     
     if(isset($arr[$class])){
-      include_once 'src/fp/' . $arr[$class];
+      include_once 'src/fp/' . $arr[$class] . '.class.php';
     }
   }
   
