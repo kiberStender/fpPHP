6f24ce95e250e3ab09195973b70f4284d895dab6
sirkleber <kleber.stender@gmail.com>
2014-11-02 11:40:11 -0200

    Created first test

--- test/fp/collections/map/MapTest.php
+++ test/fp/collections/map/MapTest.php
@@ -4,6 +4,17 @@
  *
  * @author sirkleber
  */
-class MapTest {
-  //put your code here
+class MapTest extends PHPUnit_Framework_TestCase {
+  private $mi;
+  private $md;
+  
+  public function __construct() {
+    $this->mi = Map::build(array(1, "kleber"), array(2, "eduardo"));
+    $this->md = Map::build(array(1, 2.0));
+  }
+  
+  public function testConstructor(){
+    $this->assertEquals(array(1, "kleber"), Map::build(array(1, "kleber"), array(2, "eduardo"))->head());
+  }
+  
 }
