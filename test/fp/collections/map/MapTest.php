abf6c34e825c86b8f2cb7c8b5f349d64474f2332
sirkleber <kleber.stender@gmail.com>
2014-11-02 12:22:45 -0200

    Added tests for equality and cons

--- test/fp/collections/map/MapTest.php
+++ test/fp/collections/map/MapTest.php
@@ -17,4 +17,19 @@ class MapTest extends PHPUnit_Framework_TestCase {
     $this->assertEquals(array(1, "kleber"), Map::build(array(1, "kleber"), array(2, "eduardo"))->head());
   }
   
+  public function testEquality(){
+    $this->assertEquals($this->mi, $this->mi);
+  }
+  
+  public function testEquality1(){
+    $this->assertFalse($this->mi == $this->md);
+  }
+  
+  public function testCons(){
+    $this->assertEquals(Map::build(array(1, 2.0), array(2, 3.2)), $this->md->cons(array(2, 3.2)));
+  }
+  
+  public function testCons1(){
+    $this->assertEquals(Map::build(array(1, 2.0), array(2, 3.2), array(3, 1.5)), $this->md->cons(array(3, 1.5))->cons(array(2, 3.2)));
+  }
 }
