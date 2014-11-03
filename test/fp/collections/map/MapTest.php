26a2f01e0768c618a5a0bc13458d641f94a30ce3
sirkleber <kleber.stender@gmail.com>
2014-11-03 08:06:47 -0200

    Added tests for concat, head, tail, init, last and find methods

--- test/fp/collections/map/MapTest.php
+++ test/fp/collections/map/MapTest.php
@@ -32,4 +32,38 @@ class MapTest extends PHPUnit_Framework_TestCase {
   public function testCons1(){
     $this->assertEquals(Map::build(array(1, 2.0), array(2, 3.2), array(3, 1.5)), $this->md->cons(array(3, 1.5))->cons(array(2, 3.2)));
   }
+  
+  public function testConcat(){
+    $this->assertEquals(Map::build(array(1, 2.0), array(2, 3.1)), $this->md->concat(Map::build(array(2, 3.1))));
+  }
+  
+  public function testConcat1(){
+    $this->assertEquals(Map::build(array(1, 2.0), array(2, 3.1), array(3, 2.5)), $this->md->concat(Map::build(array(3, 2.5), array(2, 3.1))));
+  }
+  
+  public function testHead(){
+    $this->assertEquals(array(1, 2.0), $this->md->head());
+  }
+  
+  public function testTail(){
+    $this->assertEquals(Map::build(array(2, "eduardo")), $this->mi->tail());
+  }
+  
+  public function testInit(){
+    $this->assertEquals(Map::build(array(1, "kleber")), $this->mi->init());
+  }
+  
+  public function testLast(){
+    $this->assertEquals(array(2, "eduardo"), $this->mi->last());
+  }
+  
+  public function testFind(){
+    $this->assertEquals(new Just(array(2, "eduardo")), $this->mi->find(new FilterAnon()));
+  }
+}
+
+class FilterAnon implements Fn1 {
+  public function apply($x) {
+    return $x[0] == 2;
+  }
 }
