c2ca01075443741581540b1c0a5798f20cb61756
Kleber Stender <kleber.stender@gmail.com>
2016-07-12 09:38:07 -0300

    Created Unit object in order to avoid null

---
+++ src/fp/utils/unit/Unit.class.php
@@ -0,0 +1,21 @@
+<?php
+
+  namespace fp\utils\unit;
+  /**
+   * Description of Unit
+   *
+   * @author sirkleber
+   */
+  class Unit {
+    private static $_unit = null;
+    private function __construct(){}
+    
+    public static function unit(){
+      if(!isset(self::$_unit)){
+        self::$_unit = new Unit();
+      }
+      
+      return self::$_unit;
+    }
+  }
+  
